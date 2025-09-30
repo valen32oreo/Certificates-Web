26c535ab6081521bb1a54a7b64e04b480eeec872
valen32oreo <rzkrisna129@gmail.com>
2025-09-30 22:03:15 +0700

    Add new files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Modal Sertifikat
+function openModal(imgSrc) {
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("modal-img").src = imgSrc;
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
